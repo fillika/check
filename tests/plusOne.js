7c1ac14c89cca927fad9a94f60ff3b62790e0730
Aleksandr Mironenko <a.mironenko@initi.ru>
2022-10-29 22:23:06 +0300

    added 3 more test

--- tests/plusOne.js
+++ tests/plusOne.js
@@ -15,3 +15,26 @@ test("use function [plusOne] with number 121", () => {
     expect(result).toBe(122);
     expect(result).not.toBe(int);
 })
+
+test("Incorrect test", () => {
+    const int = 1,
+        result = plusOne(int);
+
+    expect(result).toBe(int);
+})
+
+test("Test with Error", () => {
+    throw new Error("My custom error");
+})
+
+test("Very long test", async () => {
+    let x = 10;
+
+    await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(20)
+        }, 5000)
+    }).then(int => x = int)
+
+    expect(x).toBe(201);
+})
